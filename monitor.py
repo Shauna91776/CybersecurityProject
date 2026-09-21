ce34df617946c01a058475768070caebf6187f39
shjosjhb025 <shjosjhb025@student.wethinkcode.co.za>
2026-09-21 19:31:08 +0200

    Add file hashing and directory scanning

--- monitor.py
+++ monitor.py
@@ -1,9 +1,28 @@
 import hashlib
+from pathlib import Path
 
-text = "Hello World"
-#converts text to bytes
 
-hash_object = hashlib.sha256(text.encode())
+def calculate_hash(file_path):
+    with open(file_path, "rb") as file:
+        content = file.read()
 
-#convert to readable hexaecimal string
-print(hash_object.hexdigest())
+    hash_object = hashlib.sha256(content)
+
+    return hash_object.hexdigest()
+
+
+directory = Path("test_files")
+
+for item in directory.iterdir():
+    if item.is_file():
+        file_hash = calculate_hash(item)
+        print(item.name, file_hash)
+    
+
+# text = "Hello World"
+# #converts text to bytes
+
+# hash_object = hashlib.sha256(text.encode())
+
+# #convert to readable hexaecimal string
+# print(hash_object.hexdigest())
